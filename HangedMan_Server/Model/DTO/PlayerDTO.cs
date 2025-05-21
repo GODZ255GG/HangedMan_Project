9a259524fc1bd2101b7aaf7a22e8bc385d4b084b
René Ulises García Velázquez <reneulises_garcia@outlook.com>
2025-05-20 18:36:31 -0600

    Corrección de proyecto
    Me equivoque sobre el tipo del proyecto, pero ya agregue el que si era

---
+++ HangedMan_Server/Model/DTO/PlayerDTO.cs
@@ -0,0 +1,75 @@
+﻿using HangedMan_Server.Model.POCO;
+using System.Data.Linq;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace HangedMan_Server.Model.DTO
+{
+    public class PlayerDTO
+    {
+        public static bool registerPlayer(Player newPlayer)
+        {
+            try
+            {
+                var connection = ConnectionDB.getConnection();
+                connection.Open();
+                DataContext dataContex = new DataContext(connection);
+                dataContex.GetTable<Player>().InsertOnSubmit(newPlayer);
+                dataContex.SubmitChanges();
+                return true;
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static bool emailAlreadyRegistered(string email)
+        {
+            try
+            {
+                var connection = ConnectionDB.getConnection();
+                connection.Open();
+                DataContext dataContext = new DataContext(connection);
+                bool emailRegistered = dataContext.GetTable<Player>().Any(pl => pl.Email == email);
+                return emailRegistered;
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static bool telephoneAlreadyRegistered(string telephone)
+        {
+            try
+            {
+                var connection = ConnectionDB.getConnection();
+                connection.Open();
+                DataContext dataContext = new DataContext(connection);
+                bool telephoneRegistered = dataContext.GetTable<Player>().Any(pl => pl.PhoneNumber == telephone);
+                return telephoneRegistered;
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static bool nicknameAlreadyRegistered(string nickname)
+        {
+            try
+            {
+                var connection = ConnectionDB.getConnection();
+                connection.Open();
+                DataContext dataContext = new DataContext(connection);
+                bool nicknameRegistered = dataContext.GetTable<Player>().Any(pl => pl.NickName == nickname);
+                return nicknameRegistered;
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ HangedMan_Server/PlayerServices.svc.cs
@@ -0,0 +1,29 @@
+﻿using HangedMan_Server.Model.POCO;
+using HangedMan_Server.Model.DTO;
+using System;
+
+namespace HangedMan_Server
+{
+    public class PlayerServices : IPlayerServices
+    {
+        public bool registerPlayer(Player newPlayer)
+        {
+            return PlayerDTO.registerPlayer(newPlayer);
+        }
+
+        public bool emailAlreadyRegistered(string email)
+        {
+            return PlayerDTO.emailAlreadyRegistered(email);
+        }
+
+        public bool nicknameAlreadyRegistered(string nickname)
+        {
+            return PlayerDTO.nicknameAlreadyRegistered(nickname);
+        }
+
+        public bool telephoneAlreadyExist(string telephone)
+        {
+            return PlayerDTO.telephoneAlreadyRegistered(telephone);
+        }
+    }
+}
